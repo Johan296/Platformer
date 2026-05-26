09b39f9cc9c231c17b2df2e64f03b57fa28a661d
Johan296 <159972047+Johan296@users.noreply.github.com>
2026-05-27 10:20:28 +1200

    Gmae added to git

---
+++ platformer-/scripts/player.gd
@@ -0,0 +1,59 @@
+extends CharacterBody2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -850.0
+var alive = true
+var can_move = true
+
+func _physics_process(delta: float) -> void:
+	
+	if alive:
+		
+	
+	
+		if velocity.x > 1 or velocity.x < -1:
+			animated_sprite_2d.animation = "running"
+		else:
+			animated_sprite_2d.animation = "idle"
+
+
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+			animated_sprite_2d.animation = "jumping"
+			
+			
+		if can_move:
+			# Handle jump.
+			if Input.is_action_just_pressed("jump") and is_on_floor():
+				velocity.y = JUMP_VELOCITY
+
+			# Get the input direction and handle the movement/deceleration.
+
+			var direction := Input.get_axis("left", "right")
+			if direction:
+				velocity.x = direction * SPEED
+			else:
+				velocity.x = move_toward(velocity.x, 0, SPEED)
+
+			move_and_slide()
+
+			if direction == 1.0:
+				animated_sprite_2d.flip_h = false 
+			elif direction == -1.0:
+				animated_sprite_2d.flip_h = true 
+
+
+func die() -> void:
+	animated_sprite_2d.animation = "dying"
+	alive = false
+	
+	
+	
+
+
+func _animation_finished() -> void:
+	if animated_sprite_2d.animation == "dying":
+		await get_tree().create_timer(0.5).timeout
+		get_tree().reload_current_scene()
